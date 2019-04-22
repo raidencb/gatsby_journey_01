556b604fcdef0f0b987baf5136d5420dbad87cd8
raidencb <reydenn@callboxinc.com>
2019-04-22 09:02:44 +0800

    final tutorial 2

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,17 +3,20 @@ import { graphql } from "gatsby"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
 
+import Layout from "../components/layout"
+
 class PostTemplate extends Component {
   render() {
     const post = this.props.data.wordpressPost
 	console.log(post.content);
     return (
     	//console.log(this.props.data);
-    	<div>
-        <h1  dangerouslySetInnerHTML={{ __html: post.title }} />
-        
-        <div  dangerouslySetInnerHTML={{ __html: post.content }} />
-       </div>
+      <Layout>
+      	<div>
+          <h1  dangerouslySetInnerHTML={{ __html: post.title }} />
+          <div  dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
+      </Layout>
     )
   }
 }
