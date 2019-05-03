8b327d61281109ec1a51faf6d123ecc806e78b3d
raidencb <reydenn@callboxinc.com>
2019-05-03 13:41:06 +0800

    add pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -82,7 +82,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
                             edges: result.data.allWordpressPost.edges,
                             createPage: createPage,
                             pageTemplate: 'src/templates/index.js',
-                            pageLength: 5, // This is optional and defaults to 10 if not used
+                            pageLength: 2, // This is optional and defaults to 10 if not used
                             pathPrefix: '', // This is optional and defaults to an empty string if not used
                             context: {}, // This is optional and defaults to an empty object if not used
                         })
